b3b248407b9b291df2ef827fc9b5c59b7202ab57
TEJAS SHINDE <114345667+tejas-shinde-8022@users.noreply.github.com>
2023-07-16 20:13:09 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main()
+{
+    char hostname[1024];
+    gethostname(hostname, 1024);
+    struct hostent *host_entry;
+    host_entry = gethostbyname(hostname);
+    char *IPAddr = inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
+    std::cout << "My IP Address is: " << IPAddr << std::endl;
+    return 0;
+}
